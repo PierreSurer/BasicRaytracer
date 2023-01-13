9fb8a762d035f669a9180c4922e7cb780aee46a9
mathis <mathis.brossier@universite-paris-saclay.fr>
2023-01-13 23:27:45 +0100

    dont detach the thread!

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,11 @@
 #include <chrono>
 #include <GLFW/glfw3.h>
 #include <thread>
+#include <atomic>
 
 const int IMAGE_WIDTH = 800, IMAGE_HEIGHT = 800;
 const float WINDOW_ZOOM = 0.5f;
+std::atomic<bool> stop_signal = false;
 
 void render_loop(Image* img) {
     GLFWwindow* window;
@@ -28,8 +30,7 @@ void render_loop(Image* img) {
 
     glRasterPos2f(-1,1);
     glPixelZoom(WINDOW_ZOOM, -WINDOW_ZOOM );
-    while (!glfwWindowShouldClose(window))
-    {
+    while (!glfwWindowShouldClose(window) && !stop_signal) {
         glDrawPixels(IMAGE_WIDTH, IMAGE_HEIGHT, GL_RGBA, GL_UNSIGNED_BYTE, img->getPixels().data());
 
         glfwSwapBuffers(window);
@@ -42,19 +43,19 @@ void render_loop(Image* img) {
 
 int main(int argc, char *argv[])
 {
-    std::chrono::time_point<std::chrono::system_clock> start, end;
-    std::chrono::duration<double, std::milli> elapsed_time;
     std::cout << "Introduction to Computer Graphics - Raytracer" << std::endl << std::endl;
     if (argc < 2 || argc > 3) {
         std::cerr << "Usage: " << argv[0] << " in-file [out-file.png]" << std::endl;
         return 1;
     }
 
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    std::chrono::duration<double, std::milli> elapsed_time;
     std::shared_ptr<Scene> scene = std::make_shared<Scene>();
     Raytracer raytracer(scene);
 
-    start = std::chrono::system_clock::now();
     std::cout << "Parsing... ";
+    start = std::chrono::system_clock::now();
     if (!scene->readScene(argv[1]) || !raytracer.readParameters(argv[1])) {
         std::cerr << "Error: reading scene from " << argv[1] << " failed - no output generated."<< std::endl;
         return 1;
@@ -77,23 +78,24 @@ int main(int argc, char *argv[])
     Image img(IMAGE_WIDTH, IMAGE_HEIGHT);
 
     std::thread render_thread(render_loop, &img);
-    render_thread.detach();
 
-    start = std::chrono::system_clock::now();
     std::cout << "Tracing... ";
+    start = std::chrono::system_clock::now();
     raytracer.render(img);
     end = std::chrono::system_clock::now();
     elapsed_time = end - start;
     std::cout << (elapsed_time.count() / 1000.0) << "s" << std::endl;
 
-    start = std::chrono::system_clock::now();
     std::cout << "Writing image to " << ofname << "... ";
+    start = std::chrono::system_clock::now();
     img.writePng(ofname.c_str());
     end = std::chrono::system_clock::now();
     elapsed_time = end - start;
     std::cout << (elapsed_time.count() / 1000.0) << "s" << std::endl;
 
     std::cout << "Done." << std::endl;
+    stop_signal = true;
+    render_thread.join();
 
     return 0;
 }
